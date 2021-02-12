020558d61d6bb69348f63d292f8e4a31893307c5
Augustin Ribreau <augustin.ribreau@gmail.com>
2021-02-12 08:40:10 +0100

    fix: if/else -> conditionnel

--- src/mixins/filteredPlant.js
+++ src/mixins/filteredPlant.js
@@ -9,15 +9,10 @@ export const filteredPlant ={
     filter(action){
         const filterPlants = []
         plants.map(plant => {
-            if(action === "outside" && dateNow >= plant.start.sowing_outside && dateNow <= plant.end.sowing_outside){
-                filterPlants.push(plant);
-            }else if(action === "cover" && dateNow >= plant.start.sowing_cover && dateNow <= plant.end.sowing_cover){
-                filterPlants.push(plant);
-            } else if(action === "plantation" && dateNow >= plant.start.plant && dateNow <= plant.end.plant){
-                filterPlants.push(plant);
-            } else if(action === "harvest" && dateNow >= plant.start.harvest && dateNow <= plant.end.harvest){
-                filterPlants.push(plant);
-            }
+            action === "outside" && dateNow >= plant.start.sowing_outside && dateNow <= plant.end.sowing_outside ? filterPlants.push(plant) : [];
+            action === "cover" && dateNow >= plant.start.sowing_cover && dateNow <= plant.end.sowing_cover ? filterPlants.push(plant) : [];
+            action === "plantation" && dateNow >= plant.start.plant && dateNow <= plant.end.plant ? filterPlants.push(plant) : [];
+            action === "harvest" && dateNow >= plant.start.harvest && dateNow <= plant.end.harvest ? filterPlants.push(plant) : [];
         });
         return filterPlants;
     }
